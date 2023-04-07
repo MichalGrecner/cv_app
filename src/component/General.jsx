cbe6ecdfe539e15caad7969ab98a9ade5016e76b
Michal Grecner <grecnermichal@gmail.com>
2023-04-07 11:08:46 +0200

    improved funcionality of general part

--- src/component/General.jsx
+++ src/component/General.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useRef } from "react";
+import "./General.css"
 
 const General =()=>{
   const [genInfo, setGenInfo] = useState
@@ -19,34 +20,37 @@ const General =()=>{
     if(edit===true) setEdit(false)
   }
   const confirmHandler =()=>{
-
-    setGenInfo({
+    const newInfo={
       firstName:firstNmRef.current.value,
       lastName:lastNmRef.current.value,
       title:titleRef.current.value,
       infoText:infoRef.current.value,
-    })
-      
+    }
+
+    for (const [key, value] of Object.entries(newInfo)){
+      if (value==="") newInfo[key]=genInfo[key]
+    }
+    setGenInfo(newInfo)
     
   }
   if(edit) {
     return (
-      <>
+      <div id="general">
       <input ref={firstNmRef} type="text" placeholder={firstName}></input>
       <input ref={lastNmRef} type="text" placeholder={lastName}></input>
       <input ref={titleRef} type="text" placeholder={title}></input>
       <textarea ref={infoRef} placeholder={infoText}></textarea>
       <button onClick={()=>{editToggle(),confirmHandler()}}>Confirm</button>
-    </>
+    </div>
     )
   }
   return(
-    <>
+    <div id="general">
       <h1>{firstName} {lastName}</h1>
       <h3>{title}</h3>
       <p>{infoText}</p>
       <button onClick={editToggle}>EDIT</button>
-    </>
+    </div>
   )
 
 
