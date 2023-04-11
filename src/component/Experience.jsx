bf8af201f5eb8e9f02a90cffda376112f7f6be81
Michal Grecner <grecnermichal@gmail.com>
2023-04-11 23:39:09 +0200

    toggling between adding new experience, edit experience and show experience doesnt work as expected, but i am too tired now to make it work as intended. good night to me

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,5 +1,7 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import "./Experience.css"
+import { v4 as uuidv4 } from 'uuid';
+
 
 import ExperienceOverview from "./ExperienceOverview";
 import ExperienceEdit from "./ExperienceEdit";
@@ -7,30 +9,44 @@ import OneExperience from "./OneExperience";
 
 const Experience = () =>{
   const [experience, setExperience] = useState([
-    {id: 0, company:"firm1", position:"position1", from:2012, till:2015, desc:"description1"
-    }, 
-    {id:1, company:"firm2", position:"position2", from:2015, till:2017, desc:"description2"
-  }
+    {id:uuidv4(), company:"Company-Name", position:"position", from:"from", till:"till", desc:"job description"}
   ])
+  const [showExp, setshowExp] = useState(false)
+  const [newExp, setNewExp] = useState(true)
+  
 
+  const addNewExperience = () =>{
+    setExperience([...experience, {id:uuidv4(), company:"Company Name", position:"position", from:"from", till:"till", desc:"job description"}])
+    
 
+  }
+  
+  useEffect(() => {setNewExp(false)})
+  console.log(newExp)
   
   console.log(experience);
-  return(
-    <>
-    <h1>experience</h1>
-    {experience.map((oneExp => {
-      return (
-        <OneExperience key={oneExp.id} oneExperience={oneExp} setExperience={setExperience} experience={experience}/>
-      )
-      
-    }))}
-    <br></br>
-    <button>add new experience</button>
+  if(showExp) {
+    return(
+      <>
+      <h1>experience</h1>
+      {experience.map((oneExp => {
+        return (
+          <OneExperience key={oneExp.id} oneExperience={oneExp} setExperience={setExperience} experience={experience} newExp={newExp} />
+        )
+        
+      }))}
+      <br></br>
+      <button onClick={addNewExperience}>add new experience</button>
+      </>
+    )
+  }
+  return (
+      <>
+      <h1>experience</h1>
+      <button onClick={()=>{setshowExp(true), setNewExp(true)}}>add new experience</button>
+      </>
 
-    </>
   )
-
 
 }
 
--- src/component/OneExperience.jsx
+++ src/component/OneExperience.jsx
@@ -28,14 +28,39 @@ const OneExperience = (props) => {
     for (const [key, value] of Object.entries(newInfo)) {
       if (value === "") newInfo[key] = props.oneExperience[key];
     }
+
     let experienceCopy = [...props.experience];
-    experienceCopy[id] = newInfo;
-    props.setExperience(experienceCopy);
+
+    const updatedExperience = experienceCopy.map(oneExp => oneExp.id===newInfo.id?{...newInfo}:oneExp)
+    props.setExperience(updatedExperience);
   };
 
   const { id, company, position, from, till, desc } = props.oneExperience;
 
+
+
   if (edit) {
+    return(
+      <div id="experience-edit experience">
+      <input ref={companyRef} type="text" defaultValue={company}></input>
+      <input ref={positionRef} type="text" defaultValue={"cauky "}></input>
+      <input ref={fromRef} type="text" defaultValue={from}></input>
+      <input ref={tillRef} type="text" defaultValue={till}></input>
+      <textarea ref={descRef} defaultValue={desc}></textarea>
+      <button
+        onClick={() => {
+          editToggle(), saveHandler(id);
+        }}
+      >
+        SAVE
+      </button>
+    </div>
+
+
+    )
+  }
+
+  else if (props.newExp) {
     return (
       <div id="experience-edit experience">
         <input ref={companyRef} type="text" placeholder={company}></input>
@@ -45,7 +70,7 @@ const OneExperience = (props) => {
         <textarea ref={descRef} placeholder={desc}></textarea>
         <button
           onClick={() => {
-            editToggle(), saveHandler(id);
+             saveHandler(id);
           }}
         >
           SAVE
@@ -53,6 +78,7 @@ const OneExperience = (props) => {
       </div>
     );
   }
+  
   return (
     <div>
       <li key={id}>
