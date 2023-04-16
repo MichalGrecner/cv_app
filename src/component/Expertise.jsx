587b91cb67ffc4417c3e54a6d23ca49ac7d24cd4
Michal Grecner <grecnermichal@gmail.com>
2023-04-16 21:48:40 +0200

    skills section basic done, must be improved

--- src/component/Expertise.jsx
+++ src/component/Expertise.jsx
@@ -1,20 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 //import "./Education.css";
-
-
-
-
 
 const Expertise = () => {
   const [skills, setSkills] = useState(["skill 1", "skill 2", "skill 3"]);
   const [skillLenAux, setSkillLenAux] = useState(false)
   const [showSkills, setShowSkills] = useState(false)
+  const [edit, setEdit] = useState(false);
+  const [newSkill, setNewSkill] = useState(false)
+
+  const editToggle = () => {
+    if (edit === false) setEdit(true);
+    if (edit === true) setEdit(false);
+  };
   
 
   const addNewSkill = () => {
-    console.log("skills.push(input)")
-    ;
+
+    const skillInput = useRef()
+    setShowSkills(false)
+    setEdit(true)
+    
   };
+
+  const saveHandler = (e) => {
+    e.preventDefault();
+   
+    let skillsArray=[]
+    for (let i =0; i < e.target.length-1;i++){
+      
+      skillsArray.push(e.target[i].value)
+    }
+    setSkills(skillsArray)
+    setShowSkills(true)
+
+
+
+  }
 
   //toggles <h>Education<h> based on experience presence
   useEffect(() => {
@@ -35,14 +56,37 @@ const Expertise = () => {
           );
         })}
         <br></br>
-        <button onClick={addNewSkill}>add new skill</button>
+        <button onClick={()=>{setEdit(true); setShowSkills(false)}}>EDIT</button>
+      </>
+    );
+  }
+
+  if (edit) {
+    return (
+      <>
+        <h1>Expertise</h1>
+        <form onSubmit={saveHandler}>
+        {skills.map((oneSkill, index) => {
+          return (
+            <input key={index} type="text" defaultValue={oneSkill} />
+          );
+        })}
+        {newSkill? (
+          <input type="text" placeholder="example" />
+        ): <br></br> }
+        <button type = "submit">SAVE</button>
+        </form>
+        <br></br>
+        <button onClick={ () => {if(skills.length ===0){addNewSkill()}; setNewSkill(true)}}>add new skill</button>
+        
+
       </>
     );
   }
 
   return (
     <>
-      <button onClick={() => {setShowSkills(true); if(skills.length ===0){addNewSkill()}}}>add new skill</button>
+      <button onClick={() => {setEdit(true); }}>add new skill</button>
     </>
   );
 };
